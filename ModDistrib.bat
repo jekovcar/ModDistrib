0c72684dff5cdec498a5c8ed4e78d4bccc7486f4
jekovcar <jekovcar@gmail.com>
2026-09-15 15:47:28 +0300

    Add options Rename image of wim index(install/boot.wim)

--- ModDistrib.bat
+++ ModDistrib.bat
@@ -4,7 +4,7 @@
 REM  --> Check for permissions
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 
-REM --> If error flag set, we do not have admin.
+REM ---> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
